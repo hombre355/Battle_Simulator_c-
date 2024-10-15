f082eca015fe9fe84143e87514371f919f684e67
hombre355 <roweboat87@gmail.com>
2024-10-14 21:55:30 -0500

    fix

--- ThunderCats_Battle_Sim.cpp
+++ ThunderCats_Battle_Sim.cpp
@@ -12,7 +12,7 @@ std::string ThunderCats_Battle_Sim::battle(const ThunderCats& t1, const ThunderC
     {
         const_cast<ThunderCats&>(t2).winBattle();
         const_cast<ThunderCats&>(t1).loseBattle();
-        return t2.getName() + " wins with " + t1.getWeapon() + "!";
+        return t2.getName() + " wins with " + t2.getWeapon() + "!";
     }
     else
     {
